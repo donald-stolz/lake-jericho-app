9a059ff976bd34b383d8e5c8e11d688e37dc0e22
Donald Stolz <groot@Stolz-iMac.local>
2017-07-24 08:22:30 -0500

    First work on AddAccount & AddPerformance

---
+++ src/Components/Form/AddAccount.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+
+import AddPerformance from './AddPerformance'
+
+var accounts : [{
+      accNum: null,
+      accName: null,
+      startBal: null,
+      startDate: null,
+      tax: null,
+      horizon: null,
+      bias: null,
+      performanceHist : [{
+        date: null,
+        tax: null,
+        horizon: null,
+        bias: null,
+        beginBal: null,
+        endBal: null,
+        netReturn: null
+      }]
+    }]
+
+class AddAccount extends Component {
+
+  onSave(e){
+    e.preventDefault()
+
+  }
+
+  render(){
+
+
+    return(
+      <div className="row">
+        <div className="panel panel-primary" id="financialInformation">
+          <div className="panel-heading">
+            <h2 className="panel-title">Account Information</h2>
+          </div>
+          <div className="panel-body">
+            <form className="form-horizontal">
+
+
+              <div className="form-group"><label className="col-sm-2 control-label">Account Name:</label>
+                <div className="col-sm-10">
+                  <input type="number" className="form-control" defaultValue={account.accName}
+                      placeholder="Account Name" ref="accName"/>
+                </div>
+              </div>
+
+              <div className="form-group"><label className="col-sm-2 control-label">Start Date:</label>
+                <div className="col-sm-10">
+                  <input type="month" className="form-control" defaultValue={account.startDate}
+                      placeholder="Start Date" ref="startDate"/>
+                </div>
+              </div>
+
+              <div className="form-group"><label className="col-sm-2 control-label">Start Balance:</label>
+                <div className="col-sm-10">
+                  <input type="number" className="form-control" defaultValue={account.startBal}
+                      placeholder="Start Balance" ref="startBal"/>
+                </div>
+              </div>
+
+              <div className="form-group"><label className="col-sm-2 control-label">Tax:</label>
+                <div className="col-sm-10">
+                  <select className="custom-select form-control" id="inputTax">
+                    <option ></option>
+                    <option value="Taxable">Taxable</option>
+                    <option value="Tax-free">Tax-free</option>
+                    <option value="Tax-deffered">Tax-deffered</option>
+                  </select>
+                </div>
+              </div>
+
+              <div className="form-group"><label className="col-sm-2 control-label">Horizon:</label>
+                <div className="col-sm-10">
+                  <select className="custom-select form-control" id="inputHorizon">
+                    <option selected></option>
+                    <option value="Short">Short</option>
+                    <option value="Intermediate">Intermediate</option>
+                    <option value="Long">Long</option>
+                  </select>
+                </div>
+              </div>
+
+              <div className="form-group"><label className="col-sm-2 control-label">Bias:</label>
+                <div className="col-sm-10">
+                  <select className="custom-select form-control" id="inputBias">
+                     <option selected></option>
+                     <option value="Growth">Growth</option>
+                     <option value="Aggregation">Aggregation</option>
+                     <option value="Distribution">Distribution</option>
+                   </select>
+                </div>
+              </div>
+
+              <button onClick={this.onSave.bind(this)} className="btn btn-primary pull-right"> Continue </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default AddAccount
