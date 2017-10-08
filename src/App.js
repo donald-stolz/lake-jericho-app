f9d2a0b3e8d7cecc416e234aa7678615d9962a63
Donald Stolz <donaldtstolz@gmail.com>
2017-10-08 17:11:28 -0500

    Small changes

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
           <div className="App-header">
             <Link to="/"><h1> Lake Jericho </h1></Link>
           </div>
-          <div className="row" >
+          <div className="row">
             <Route exact path="/" component={Home}/>
             <Route path="/NewClient" component={NewClient}/>
             <Route path="/Profile/:id" component={ProfilePage}/>
